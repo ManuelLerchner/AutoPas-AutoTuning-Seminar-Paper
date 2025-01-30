804dfc53eadd6ab14b4e3b7e85c04d21c4db7806
ManuelLerchner <manuel.lerchner1111@gmail.com>
2025-01-30 19:10:08 +0100

    add evidence data

--- data/launch.py
+++ data/launch.py
@@ -1,14 +1,13 @@
-#!/usr/bin/env python3
-
 import subprocess
 import os
 from time import sleep
+import shutil
 
 SCRIPT_TEMPLATE = """#!/bin/bash
 
-#SBATCH -J FuzzyTuning_{{YAML_FILENAME}}_Threads
+#SBATCH -J EarlyStopping_{{YAML_FILENAME}}_Threads
 #SBATCH -o ./%x.%j.%N.out
-#SBATCH -D ./AutoPas/build/examples/md-flexible
+#SBATCH -D ./AutoPas/build/examples/md-flexible/{{SCENARIO_NAME}}/{{FACTOR}}
 #SBATCH --get-user-env
 #SBATCH --clusters={{CLUSTER}}
 #SBATCH --partition={{PARTITION}}
@@ -20,18 +19,20 @@ SCRIPT_TEMPLATE = """#!/bin/bash
 #SBATCH --time=06:00:00
 
 sleep $((RANDOM % 120))
-./md-flexible --yaml-filename {{YAML_FILENAME}} --log-level info {{PARAMS}}
+../../md-flexible --yaml-filename {{YAML_FILENAME}} --log-level info {{PARAMS}}
 """
 
 
-def launch_job(scenario_file, params, factor, cluster, partition):
+def launch_job(scenario, yaml, params, factor, cluster, partition):
     script = SCRIPT_TEMPLATE
     script = script.replace("{{CLUSTER}}", cluster)
     script = script.replace("{{PARTITION}}", partition)
-    script = script.replace("{{YAML_FILENAME}}", scenario_file)
+    script = script.replace("{{SCENARIO_NAME}}", scenario)
+    script = script.replace("{{YAML_FILENAME}}", yaml)
+    script = script.replace("{{FACTOR}}", factor)
     script = script.replace("{{PARAMS}}", params)
 
-    scenario_name = scenario_file.split(".")[0]
+    scenario_name = yaml.split(".")[0]
     filename = scenario_name+"_"+params+"_"+str(factor)+".sh"
     with open(filename, "w") as f:
         f.write(script)
@@ -60,16 +61,29 @@ if __name__ == "__main__":
     printUsefullCommands()
     factor = ["1.25", "1.5", "2", "2.5", "3", "5", "7.5", "10",
               "12.5", "15", "20", "25", "250", "100", "500", "1000", "10000"]
-    factor = ["7.5", "inf"]
-
+    factor = ["2"]
     scenario_name = "explodingLiquid"
 
     for factor in factor:
 
         cluster, partition = ("serial", "serial_std")
 
+        # replace factor in new file
         new_scenario_name = scenario_name.split(
             ".")[0] + "-" + factor+".yaml"
 
-        launch_job(new_scenario_name, "",
+        pathname = "md-flexible/"+scenario_name+"/"+factor
+        os.makedirs(pathname, exist_ok=True)
+
+        with open("md-flexible/"+scenario_name+".yaml", "r") as f:
+            file = f.read()
+            file = file.replace("max-allowed-slowdown-factor      :  {}",
+                                f"max-allowed-slowdown-factor      :  {factor}")
+
+            with open(pathname+"/"+new_scenario_name, "w") as f2:
+                f2.write(file)
+
+        # mkdir
+
+        launch_job(scenario_name, new_scenario_name, "",
                    factor, cluster, partition)
